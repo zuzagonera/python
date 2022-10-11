db458c138131c4f0e5eae7e210404866ddef159c
zuzagonera <zuzagonera.contact@gmail.com>
2022-10-11 23:52:47 +0200

    Lab_2 zad_2c

--- zad_2c.py
+++ zad_2c.py
@@ -1,8 +1,7 @@
-def numbers(number, number1, number2, number3, number4, number5, number6, number7, number8, number9):
-    numbers_list = [number, number1, number2, number3, number4, number5, number6, number7, number8, number9]
-    for number in numbers_list:
-        if number % 2 == 0:
-            print(number)
+def numbers(numbers):
+    for number in range(len(numbers)):
+        if numbers[number] % 2 == 0:
+            print(numbers[number])
 
 
-numbers(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+numbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
