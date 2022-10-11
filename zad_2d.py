0bc9968b14ed6880eedf76d12facacf678937722
zuzagonera <zuzagonera.contact@gmail.com>
2022-10-11 17:33:31 +0200

    Lab 2 zad_2

---
+++ zad_2d.py
@@ -0,0 +1,6 @@
+def numbers(number, number1, number2, number3, number4, number5, number6, number7, number8, number9):
+    numbers_list = [number, number1, number2, number3, number4, number5, number6, number7, number8, number9]
+    print(numbers_list[::2])
+
+
+numbers(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
