f8bd2b075824e254c3bf904ace138258c9b98b71
zuzagonera <zuzagonera.contact@gmail.com>
2022-10-11 17:02:36 +0200

    Lab_2 zad_2

---
+++ zad_2bi.py
@@ -0,0 +1,8 @@
+def numbers(number, number1, number2, number3, number4):
+    numbers_list = [number, number1, number2, number3, number4]
+    for number in numbers_list:
+        number = number * 2
+        print(number)
+
+
+numbers(0, 1, 2, 3, 4)
